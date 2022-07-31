6f5953dd96e5396275e93d71f4a7f8949617a6b1
Elena <15el.shev@gmail.com>
2022-07-31 19:09:00 +0300

    Add clear input field

--- src/components/ToDo/ToDoForm/index.js
+++ src/components/ToDo/ToDoForm/index.js
@@ -5,12 +5,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusSquare } from "@fortawesome/free-solid-svg-icons";
 
 const ToDoForm = ({ addTask }) => {
+  const onSubmit = (value, formikBag) => {
+    formikBag.resetForm();
+    addTask(value);
+  };
   return (
     <div className={styles.form_todo}>
-      <Formik initialValues={{ body: "" }} onSubmit={addTask}>
+      <Formik
+        initialValues={{ body: "", isDone: false }}
+        onSubmit={onSubmit}
+      >
         <Form>
           <Field name="body" className={styles.form_input} />
-          <button type="submit" className={styles.form_button}>
+          <button type="submit" value="text" className={styles.form_button}>
             <FontAwesomeIcon icon={faPlusSquare} />
           </button>
         </Form>
